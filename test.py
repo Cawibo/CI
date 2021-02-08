07383f5431868e63457d471208468ce768c48fff
JakobBerggren <jaberggr@gmail.com>
2021-02-08 10:28:13 +0100

    fixed minor issue in test

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
-    
+
     def test_index(self):
-        self.assertEqual("Hello",app.index())
+        self.assertEqual("Hello :)",app.index())
 
 
 if __name__ == '__main__':
